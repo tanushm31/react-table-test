19870bf761ea3bb2bd8dd17635818615ee58fa7a
Tanush Mahajan <tanush.mohina@gmail.com>
2022-12-10 18:40:42 +0530

    Working Table with Searching, Sorting & Filtering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import { type NextPage } from "next";
 // import ReactTable from "@tanstack/react-table";
 // import "@tareact-table/react-table.css";
 import Table from "../components/Table";
-
+import Table3 from '../components/Table3'
 const Home: NextPage = () => {
   return (
     <div className="h-screen w-screen bg-gray-800 text-white">
       <Table/>
+      <div className="w-full h-[50px] bg-slate-400 font-bold text-xl">TABLE 3</div>
+      <div className="w-full bg-white p-5 text-black border-blue-500 border-[10px]"><Table3 /></div>
     </div>
   );
 };
