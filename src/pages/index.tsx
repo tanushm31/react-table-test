7566f3bbb5c4326e0a91a0836a821cbc86b7562e
Tanush Mahajan <tanush.mohina@gmail.com>
2022-12-10 14:09:37 +0530

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { type NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-screen w-screen bg-gray-800 text-white">
+
+    </div>
+  );
+};
+
+export default Home;
