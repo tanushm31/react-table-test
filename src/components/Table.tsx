d6585777ded82b859aff4bc82832cc210627e7bb
Tanush Mahajan <tanush.mohina@gmail.com>
2022-12-10 17:01:52 +0530

    basic table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,8 +1,148 @@
-import React from 'react'
+import * as React from 'react'
 
-export default function Table() {
+// import './index.css' 
+
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+
+type Person = {
+  firstName: string
+  lastName: string
+  age: number
+  visits: number
+  status: string
+  progress: number
+}
+// Just some dummy data
+const defaultData: Person[] = [
+  {
+    firstName: 'tanner',
+    lastName: 'linsley',
+    age: 24,
+    visits: 100,
+    status: 'In Relationship',
+    progress: 50,
+  },
+  {
+    firstName: 'tandy',
+    lastName: 'miller',
+    age: 40,
+    visits: 40,
+    status: 'Single',
+    progress: 80,
+  },
+  {
+    firstName: 'joe',
+    lastName: 'dirte',
+    age: 45,
+    visits: 20,
+    status: 'Complicated',
+    progress: 10,
+  },
+]
+
+// Create a helper to create columns !IMPORTANT
+const columnHelper = createColumnHelper<Person>()
+
+const columns = [
+  columnHelper.accessor('firstName', {
+    id: 'firstName',
+    header: () => 'First Name',
+    cell: info => info.getValue(),
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor(row => row.lastName, {
+    id: 'lastName',
+    cell: info => <i>{info.getValue()}</i>,
+    header: () => <span>Last Name</span>,
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('age', {
+    header: () => 'Age',
+    cell: info => info.renderValue(),
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('visits', {
+    header: () => <span>Visits</span>,
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('status', {
+    header: 'Status',
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('progress', {
+    header: 'Profile Progress',
+    footer: info => info.column.id,
+  }),
+  
+
+]
+
+function Table() {
+  const [data, setData] = React.useState(() => [...defaultData])
+  const rerender = React.useReducer(() => ({}), {})[1]
+
+  const table = useReactTable({
+    data,
+    columns,
     
+    
+    /* This required option is a factory 
+    for a function that computes and returns the core row model for the table. 
+    It is called once per table and should return a new function 
+    which will calculate and return the row model for the table. */
+    getCoreRowModel: getCoreRowModel(), 
+  })
+
   return (
-    <div>Table</div>
+    <div className="p-2">
+      <table 
+      className='
+        w-full
+        h-full
+        border-collapse
+        border-2 border-white
+        text-white
+        text-sm'
+        >
+        <thead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <th key={header.id} className="border-2 border-white h-[60px] font-bold text-xl">
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map(row => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <td key={cell.id} className="border-2 border-white h-[100px] p-2">
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="h-4" />
+      {/* <button onClick={() => rerender()} className="border p-2">
+        Rerender
+      </button> */}
+    </div>
   )
 }
+
+export default Table
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { type NextPage } from "next";
 // import ReactTable from "@tanstack/react-table";
 // import "@tareact-table/react-table.css";
-
+import Table from "../components/Table";
 
 const Home: NextPage = () => {
   return (
     <div className="h-screen w-screen bg-gray-800 text-white">
-
+      <Table/>
     </div>
   );
 };
